27bb36c59d90a6f7b912375ae9a44de97d42ed7b
Cyle Dawson <dawsonc623@gmail.com>
2016-02-16 23:09:50 -0600

    Updates inline documentation to be more accurate.

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,7 +2,6 @@ function animatePoints(points) {
     function revealPoint(point, delay) {
         "use strict";
 
-        // Create a "staggered" animation effect
         point.style.transitionDelay = delay + "s";
         point.style.msTransitionDelay = delay + "s";
         point.style.WebkitTransitionDelay = delay + "s";
@@ -12,7 +11,8 @@ function animatePoints(points) {
         point.style.msTransform = "scaleX(1) translateY(0)";
         point.style.WebkitTransform = "scaleX(1) translateY(0)";
     }
-    
+
+    // Create a "staggered" animation effect
     forEach(points, function(point, index) {
         revealPoint(point, 0.2 * (index + 1));
     });
@@ -20,11 +20,11 @@ function animatePoints(points) {
 
 window.onload = function () {
     "use strict";
-    
+
     var points = document.getElementsByClassName("point"),
         pointsContainer = document.querySelector(".selling-points"),
         scrollThreshold = pointsContainer.getBoundingClientRect().top - window.innerHeight + 200;
-    
+
     // In case the points are within visible range already
     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollThreshold) {
         animatePoints(points);
