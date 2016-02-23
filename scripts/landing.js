da08ab8ac0850d78898f27f79dc0732d1f3ba62d
Cyle Dawson <dawsonc623@gmail.com>
2016-02-23 00:00:51 -0600

    Updates Landing view to use jQuery.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,39 +1,32 @@
 function animatePoints(points) {
-    function revealPoint(point, delay) {
-        "use strict";
-
-        point.style.transitionDelay = delay + "s";
-        point.style.msTransitionDelay = delay + "s";
-        point.style.WebkitTransitionDelay = delay + "s";
-
-        point.style.opacity = 1;
-        point.style.transform = "scaleX(1) translateY(0)";
-        point.style.msTransform = "scaleX(1) translateY(0)";
-        point.style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-
-    // Create a "staggered" animation effect
-    forEach(points, function(point, index) {
-        revealPoint(point, 0.2 * (index + 1));
+    $.each(points, function(index, point) {
+        $(point).css({
+            "opacity": 1,
+            "transform": "scaleX(1) translateY(0)",
+            // "stagger" the transitions
+            "transitionDelay": 0.2 * (index + 1) + "s"
+        });
     });
 }
 
-window.onload = function () {
+$(window).load(function () {
     "use strict";
 
     var points = document.getElementsByClassName("point"),
-        pointsContainer = document.querySelector(".selling-points"),
-        scrollThreshold = pointsContainer.getBoundingClientRect().top - window.innerHeight + 200;
+        scrollThreshold,
+        $window = $(window);
+    
+    scrollThreshold = $(".selling-points").offset().top - $window.height() + 200;
 
     // In case the points are within visible range already
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollThreshold) {
+    if ($window.scrollTop() >= scrollThreshold) {
         animatePoints(points);
     }
     else {
-        window.addEventListener("scroll", function(event) {
-            if (document.documentElement.scrollTop || document.body.scrollTop >= scrollThreshold) {
+        $(window).scroll(function(event) {
+            if ($window.scrollTop() >= scrollThreshold) {
                 animatePoints(points);
             }
         });
     }
-};
+});
